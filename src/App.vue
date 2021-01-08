be728de67793313b946648d9ac1a294f13fe09f0
yoga-au <yogaagung.utama@gmail.com>
2021-01-08 17:48:05 +0700

    remove: default browser padding, margin, box-sizing, add: style to start screen

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Hello Vue</h1>
+  <div class="start-screen">
+    <h1 class="start-screen__title">Reaction Game</h1>
+    <p class="start-screen__sub-title">Test your reaction time</p>
+    <div class="start-screen__start-btn">start game</div>
+  </div>
 </template>
 
 <script>
@@ -16,8 +20,33 @@ export default {
   font-family: 'Press Start 2P', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  /* padding-top: 50vh; */
+}
+.start-screen {
+  height: 100vh;
+  color: white;
+  background-color: #171c22;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 30vh;
+}
+.start-screen__title{
+  margin-bottom: 24px;
+}
+.start-screen__sub-title{
+  opacity: .4;
+  margin-bottom: 80px;
+}
+.start-screen__start-btn{
+  border: 1px solid white;
+  text-transform: uppercase;
+  width: 250px;
+  margin: 0 auto;
+  padding: 24px;
+  transition: all .2s ease-out;
+}
+.start-screen__start-btn:hover{
+  border: 1px solid white;
+  background-color: white;
+  color: black;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@fontsource/press-start-2p'
+import './assets/global.css'
 
 createApp(App).mount('#app')
